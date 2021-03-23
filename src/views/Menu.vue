234f10fb28e55ec30b82547121f927b04b12da0f
yskim-web <abcm556@naver.com>
2021-03-23 11:31:01 +0900

    Invalid extends configuration value

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -9,15 +9,49 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import * as kafka from 'kafka-node';
+// import { EventEmitter } from 'events';
+// EventEmitter.defaultMaxListeners = 0;
 export default class Menu extends Vue {
-    public data(){
+  public data(){
         return {
-            selectedMenu : undefined,
-
+            selectedMenu : "",
+            randomNum : {
+                random : function(n1, n2):number{ return Math.random() * (n2 -n1 +1) + n1 },
+                auth : function(n):number{
+                       var value = 0;
+                        for(var i=0; i<n; i++){
+                            value += this.random(0,9);
+                        }
+                        return value;
+                },
+            }
         }
     }
     public mounted() {
-        console.log(this);
+        this.init();
+    }
+    get randomNum() : object {
+        return this.randomNum
+    }
+    public init():void{
+        this.randomNum
+    }
+    public makeWASIP() : string {
+        return (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
+    }
+    public generateRandom(max : number ,min : number) : number{
+        return Math.floor(Math.random()*(max-min+1)) + min;
+    }
+    public makeUUID(){
+         return  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      }).replace(/-/g,"");
+    }
+    public randomArray(arr : Array<any>) : Array<any> {
+        return arr[Math.floor(Math.random()*arr.length)]
     }
 }
+
 </script>
